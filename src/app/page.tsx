cea862127fb0fbd7bb1c4f4d46c56043b2fe0e4d
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-09-17 00:21:31 +0600

    [ADDED]: Bob added his code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div>
         <h2>Hello world</h2>
+
+        {/* Bob's code here */}
+        <p>
+            Hi, this is Mr. Bob
+        </p>
     </div>
   );
 }
