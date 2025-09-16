52586213a3c89007a22c29b5294eb96dab282600
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-09-17 00:49:49 +0600

    alex updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-        <h2>Hello world</h2>
-
         {/* Alex's code here */}
         <p>Hi, I am alex. I live in NYC</p>
     </div>
